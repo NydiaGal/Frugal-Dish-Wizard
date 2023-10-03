b16e937fe9a382b4b739a606b28024c7a825f25d
Rainy <142264832+MzRainy@users.noreply.github.com>
2023-10-02 20:58:29 -0500

    api code

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,31 @@ var mealType ='';
 var favorite = '';
 var newRecipe='';
 
+var SpoonacularApi = require('spoonacular_api');
+
+var defaultClient = SpoonacularApi.ApiClient.instance;
+// Configure API key authorization: apiKeyScheme
+var apiKeyScheme = defaultClient.authentications['apiKeyScheme'];
+apiKeyScheme.apiKey = "YOUR API KEY"
+// Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
+//apiKeyScheme.apiKeyPrefix['x-api-key'] = "Token"
+
+var api = new SpoonacularApi.DefaultApi()
+var analyzeRecipeRequest = new SpoonacularApi.AnalyzeRecipeRequest(); // {AnalyzeRecipeRequest} Example request body.
+var opts = {
+  'language': en, // {String} The input language, either \"en\" or \"de\".
+  'includeNutrition': false, // {Boolean} Whether nutrition data should be added to correctly parsed ingredients.
+  'includeTaste': false // {Boolean} Whether taste data should be added to correctly parsed ingredients.
+};
+var callback = function(error, data, response) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('API called successfully. Returned data: ' + data);
+  }
+};
+api.analyzeRecipe(analyzeRecipeRequest, opts, callback);
+
 //-Function to make api request including api key
 //	-make fetch request
 //-Function to display recipe cards
